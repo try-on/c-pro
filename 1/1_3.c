98190154b12e30ab85a3fae733ad93fd88cace94
try-on <56321609+try-on@users.noreply.github.com>
2019-10-08 23:16:02 +0300

    Create 1_3.c

---
+++ 1/1_3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+   double V;
+   double T;
+   double cost;
+   double I;
+   
+   scanf("%lf %lf", &T, &V);
+   
+   I = (T * V) / 1024;
+   cost = (I - 1) * 10;
+   
+   printf("%.0f %.0f", I, cost);
+   
+    
+    
+    
+    
+    
+    
+    
+    
+}
