20d2cda3f54282d5b3a0ca4f6f323e7fa15aabc6
try-on <56321609+try-on@users.noreply.github.com>
2019-10-20 19:21:59 +0300

    Create 2_5.c

---
+++ 2_5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+   int a1;
+   int b1;
+   int a2;
+   int b2;
+   
+   scanf("%1d %1d %1d %1d", &a1, &b1, &a2, &b2);
+   
+   if (a1 == a2 || b1 == b2) {
+       
+       printf("YES");
+       
+   }
+   
+   
+   
+   
+   else {
+         printf("NO");
+         
+     }
+     
+   
+   
+   
+    return 0;
+}
