8c00cd99babe81b7f3a22e5d7ecdfcb6054754bf
try-on <56321609+try-on@users.noreply.github.com>
2019-11-25 18:15:53 +0300

    Create 6_1.c

---
+++ 6_1.c
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+struct point {
+    int x,y;
+};
+int main()
+{
+ 
+ map < point, string > names;
+ point a;
+ for(int i = 0; i < n; i++){
+     cin >> a.x >> a.y;
+     string name;
+     cin >> name;
+     names[a] = name;
+     
+ } 
+ point b;
+ cin >> b.x >> b.y;
+ cout << names[b];
+ 
+  
+}
