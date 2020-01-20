2138ed0817452492b11c4f2b0a3a8b90d736854f
try-on <56321609+try-on@users.noreply.github.com>
2020-01-20 18:27:28 +0300

    Create 7_1.c

---
+++ 7_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+     void tmp (const string & name, 
+            const string & secondName ){
+        //oyi8hgh8uyhuhyhh8
+        cout << 2;
+            }
+            
+        void tmp(const string & name,
+            const string & secondName, 
+           const string & salary) {
+           ///okjoihuhuiohiuohouih0
+           cout << 3;
+    }
+
+int main(){
+    
+    //tmp("twoWords", "chto-to", "plus");
+    tmp("twoWords", "andonemore");
+    // ja nihera ne ponimayuuu
+    
+    
+}
